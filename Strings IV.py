9bb41fdf21eeee5aec198e16811aaa774191237d
GERARDOG-22310244 <144291354+GERARDOG-22310244@users.noreply.github.com>
2025-03-16 19:48:20 +0000

    Saltos de linea y tabulacion

---
+++ Strings IV.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ coding: 850  _*_
+# _*_ coding: utf-8 _*_
+
+print("Lenguajes de programacion" + "\n\tPython\n\tJavaScript\n\tJava\n\tPHP\n\tTypeScript")
+
