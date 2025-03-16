9131b3c5721a06ad182bb90b5e78b53304f57dd9
GERARDOG-22310244 <144291354+GERARDOG-22310244@users.noreply.github.com>
2025-03-16 19:33:51 +0000

    Strings II

---
+++ Strings II.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# _*_ coding: cp1252 _*_
+# _*_ coding: 850  _*_
+# _*_ coding: utf-8 _*_
+
+#PrimeraPalabra = "Programacion"
+
+#SegundaPalabra = "Facil"
+
+#NombreCanal = PrimeraPalabra + " " + SegundaPalabra + "."
+
+print("Este canal de " + "Programacion Facil.")
