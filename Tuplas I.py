da5a7644ed98b3721b771f1a8702e49b83c36032
GERARDOG-22310244 <144291354+GERARDOG-22310244@users.noreply.github.com>
2025-03-16 21:38:27 +0000

    no se modifica

---
+++ Tuplas I.py
@@ -0,0 +1,11 @@
+#lista = ['Rojo', 'Azul', 'Amarillo', 'Verde']
+
+tupla = (10, 15, 20, "El resultado de la operacion es: ")
+
+tupla1 = (10,)
+
+#lista.append(50)
+
+#print(tupla[3], tupla[0] + tupla[2])
+
+#print(lista)
