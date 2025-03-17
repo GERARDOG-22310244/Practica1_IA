a749d18f2b63201ac1f950a92fc21a6ba4ab82d1
GERARDOG-22310244 <144291354+GERARDOG-22310244@users.noreply.github.com>
2025-03-17 01:29:47 +0000

    Esta o no en la lista

---
+++ Comprobar datos de listas y tuplas.py
@@ -0,0 +1,10 @@
+entrada = input("introduce el nombre de un navegador:\n")
+
+navegadores = ['chrome', 'firefox', 'opera', 'safari']
+
+if entrada in navegadores:
+    print("El navegador que buscas esta en la lista")
+
+else:
+    print("El navegador que buscas no esta en la lista")
+
