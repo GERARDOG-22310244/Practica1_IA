22be4ffe0743f36adcf543d6f974095088be91b1
GERARDOG-22310244 <144291354+GERARDOG-22310244@users.noreply.github.com>
2025-03-23 20:48:36 +0000

    Diccionario II

---
+++ DICCIONARIOS/Diccionarios II.py
@@ -0,0 +1,14 @@
+teclado1 = {
+    "Categoria": "Teclados",
+    "Modelo": "HyperX Alloy FPS Pro",
+    "Precio": "89,99"
+    }
+
+teclado2 = {
+    "Categoria": "Teclados",
+    "Modelo": "Corsair k55 RGB",
+    "Precio": "59,99"
+    }
+
+for x, y in teclado2.items():
+    print(x, ":", y)
