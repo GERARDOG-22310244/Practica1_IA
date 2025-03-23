70548dc45cb433e1d243c15af4840cb7b0f85f11
GERARDOG-22310244 <144291354+GERARDOG-22310244@users.noreply.github.com>
2025-03-23 20:33:51 +0000

    Diccionario 1

---
+++ DICCIONARIOS/Diccionarios I.py
@@ -0,0 +1,18 @@
+teclado1 = {
+    "Categoria": "Teclados",
+    "Modelo": "HyperX Alloy FPS Pro",
+    "Precio": "89,99"
+    }
+
+teclado2 = {
+    "Categoria": "Teclados",
+    "Modelo": "Corsair k55 RGB",
+    "Precio": "59,99"
+
+    }
+
+muestrateclado = dict(teclado1)
+print(muestrateclado)
+
+consulta = teclado1["Modelo"], teclado1["Precio"], teclado2["Modelo"], teclado2["Precio"]
+
